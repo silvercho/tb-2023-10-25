832325a9597533e8e557fdb357e86ab7573e4354
silvercho <tjddms01212@gmail.com>
2023-10-25 14:25:53 +0900

    세팅

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class Main {
+    public static void main(String[] args) {
+        new App().run();
+        System.out.println("Hello world!");
+    }
+}
+class App {
+    void run(){
+
+    }
+}
